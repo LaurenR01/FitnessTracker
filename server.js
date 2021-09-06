e068747acdbb72af7b7a03b05bb22844272f586d
LaurenR01 <laurenrowe01@gmail.com>
2021-09-06 15:35:42 -0400

    setting up models

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const logger = require( 'morgan');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-const db = require('./models');
+const db = require("./models");
 
 const app = express();
 
@@ -15,7 +15,8 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODDB_URI || "mongod://localhost/populaterdb", {useNewUrlParser : true});
+mongoose.connect(process.env.MONGODDB_URI || "mongod://localhost:27017/workout", {useNewUrlParser : true});
+s
 
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}.`);
