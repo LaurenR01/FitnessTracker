b868c9982cc963e6d0f06be63225b2fd7328fee3
LaurenR01 <laurenrowe01@gmail.com>
2021-09-10 11:14:02 -0400

    working on routes

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,19 @@ const Workout = require("../models/index.js");
 const express = require('express');
 const router = express.Router();
 
+exports.getWorkouts = async (req, res) => {
+    const workouts = await Workout.find();
+
+    res.status(200).json({
+        status: 'success',
+        result_num: workouts.length,
+        data: workouts
+    })
+};
+
+    
 router
     .route('/workouts')
-    .get()
+    .get(this.getWorkouts)
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const logger = require( 'morgan');
+const dotenv = require('dotenv').config();
 const mongoose = require('mongoose');
 const apiRoute = require("./routes/api");
 const htmlRoute = require("./routes/html");
+const routeIndex = require('./routes/index');
 
-const PORT = process.env.PORT || 3001;
-
-const db = require("./models");
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 const app = express();
 
@@ -16,11 +16,15 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODDB_URI || "mongod://localhost/workout", {useNewUrlParser : true})
-.then(con => {
-    console.log("Database connected successfully")
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,   
+}).then(con => {
+    console.log("Database successfully connected")
 });
 
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Application running on port ${PORT}.`);
 });
